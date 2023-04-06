120f895ebee27ea58ae8b1ce80a9f1cc21d0640e
Md Yusuf <mdyusuf742171@gmail.com>
2023-04-06 21:08:51 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/Xe-4OdWrGgLBNb-0LP5LtC1PsoV2ocbo',
-      accounts: ['a4048ca1d9b55081ec8c4a24e2b4588f3a46c9a5414d09ab3883615c6fb8db88']
+      accounts: ['put your wallet secret key']
     }
   }
 };
